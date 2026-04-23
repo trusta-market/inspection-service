87b168257483a1b218a67bcb571a0b492e273296
Seungwon-Choi <bestwondev@gmail.com>
2026-04-23 14:26:40 +0900

    chore: Spring Boot 프로젝트 초기화

---
+++ src/main/java/com/trustamarket/inspectionservice/InspectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trustamarket.inspectionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspectionServiceApplication.class, args);
+    }
+
+}
