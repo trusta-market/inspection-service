77863cf715e09b8c8c52c9ae1220213d0d0fe543
Seungwon-Choi <bestwondev@gmail.com>
2026-04-26 18:27:34 +0900

    fix: 센터 중복 검증 로직 추가

--- src/main/java/com/trustamarket/inspectionservice/center/adapter/out/persistence/jpa/InspectionCenterJpaRepository.java
+++ src/main/java/com/trustamarket/inspectionservice/center/adapter/out/persistence/jpa/InspectionCenterJpaRepository.java
@@ -1,10 +1,24 @@
 package com.trustamarket.inspectionservice.center.adapter.out.persistence.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface InspectionCenterJpaRepository extends JpaRepository<InspectionCenterJpaEntity, UUID> {
+
     Optional<InspectionCenterJpaEntity> findByCenterIdAndDeletedAtIsNull(UUID centerId);
+
+    @Query("""
+            SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END
+                FROM InspectionCenterJpaEntity c
+                WHERE c.name = :name AND c.addressLine1 = :addressLine1 AND c.postalCode = :postalCode AND c.deletedAt IS NULL
+            """)
+    boolean existsByNameAndAddressLine1AndPostalCode(
+            @Param("name") String name,
+            @Param("addressLine1") String addressLine1,
+            @Param("postalCode") String postalCode
+    );
 }
