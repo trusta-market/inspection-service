525d10edeebe7d06806aaab9c4ef970672f71d39
Seungwon-Choi <bestwondev@gmail.com>
2026-05-09 17:05:26 +0900

    feat: Column 어노테이션 명시

--- src/main/java/com/trustamarket/inspectionservice/center/adapter/out/persistence/jpa/InspectionCenterJpaEntity.java
+++ src/main/java/com/trustamarket/inspectionservice/center/adapter/out/persistence/jpa/InspectionCenterJpaEntity.java
@@ -67,8 +67,10 @@ public class InspectionCenterJpaEntity implements Persistable<UUID> {
     @Column(nullable = false)
     private Instant updatedAt;
 
+    @Column
     private Instant deletedAt;
 
+    @Column
     private String deletedBy;
 
     @Transient
